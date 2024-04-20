3e572b8c7e7b32cae78f20e2d2a72b02df7c2273
Thomas Lindbäck <thomas.lindback@gmail.com>
2024-04-20 19:34:52 +0000

    server ready

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -42,13 +42,17 @@ bool s_is_open = false;
 static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
 {
     switch (event->Type) {
+    case chip::DeviceLayer::DeviceEventType::kServerReady:
+        ESP_LOGI(TAG, "-> Server Ready <-");
+        esp_timer_start_periodic(s_reporting_timer, (uint64_t)30000000ULL);
+        break;
     case chip::DeviceLayer::DeviceEventType::kInterfaceIpAddressChanged:
         ESP_LOGI(TAG, "Interface IP Address changed");
         break;
 
     case chip::DeviceLayer::DeviceEventType::kCommissioningComplete:
         ESP_LOGI(TAG, "Commissioning complete");
-        esp_timer_start_periodic(s_reporting_timer, (uint64_t)30000000ULL);
+        //esp_timer_start_periodic(s_reporting_timer, (uint64_t)30000000ULL);
         break;
 
     case chip::DeviceLayer::DeviceEventType::kFailSafeTimerExpired:
